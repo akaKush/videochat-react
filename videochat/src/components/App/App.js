0df63fa099b012ba0766c68229fc5e3783568fdb
marc <marcbosch90@gmail.com>
2020-11-27 14:43:50 +0100

    imports + constants necessàries, i iniciem la main==APP() function

---
+++ videochat/src/components/App/App.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState, useCallback } from 'react';
+import Call from '../Call/Call';
+import StartButton from '../StartButton/StartButton';
+import api from '../../api';
+import './App.css';
+import Tray from '../Tray/Tray';
+import CallObjectContext from '../../CallObjectContext';
+import { roomUrlFromPageUrl, pageUrlFromRoomUrl } from '../../urlUtils';
+import DailyIframe from '@daily-co/daily-js';
+import { logDailyEvent } from '../../logUtils';
+
+const STATE_IDLE = 'STATE_IDLE';
+const STATE_CREATING = 'STATE_CREATING';
+const STATE_JOINING = 'STATE_JOINING';
+const STATE_JOINED = 'STATE_JOINED';
+const STATE_LEAVING = 'STATE_LEAVING';
+const STATE_ERROR = 'STATE_ERROR';
+
+export default function App() {
+  const [appState, setAppState] = useState(STATE_IDLE);
+  const [callObject, setCallObject] = useState(null);
+
+  
+}
